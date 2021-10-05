0c91aca8b3c8ddb46414fb4e411e7c8b0e4888ae
PeshoBiceps <peshowww@gmail.com>
2021-10-05 23:54:38 +0300

    .

--- pages/profile.js
+++ pages/profile.js
@@ -1,13 +1,15 @@
 import Layout from "../components/Layout"
 import Image from 'next/image'
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { FaRegUser } from 'react-icons/fa'
 import { IoShirtOutline } from 'react-icons/io5'
 import { BiExit } from 'react-icons/bi'
+import { setUserLogOut } from "../features/user/userSlice"
 
 const Profile = () => {
 
   const user = useSelector(state => state.user)
+  const dispatch = useDispatch()
 
   console.log(user)
 
@@ -16,9 +18,9 @@ const Profile = () => {
       <div className='max-w-[900px] mx-auto mt-10 p-8 sm:p-0'>
         <div className='flex flex-wrap justify-between'>
           <div className='mt-3 space-y-2'>
-            <p className='flex items-center border-b-2 text-gray-500 hover:text-black hover:border-black transition ease-in-out delay-75 sm:cursor-pointer'><FaRegUser />My Profile</p>
-            <p className='flex items-center border-b-2 text-gray-500 hover:text-black hover:border-black transition ease-in-out delay-75 sm:cursor-pointer'><IoShirtOutline />My Orders</p>
-            <p className='flex items-center border-b-2 text-gray-500 hover:text-black hover:border-black transition ease-in-out delay-75 sm:cursor-pointer'><BiExit />Log out</p>
+            <p className='flex items-center border-b-2 rounded-sm text-gray-500 hover:text-black hover:border-black transition ease-in-out delay-75 sm:cursor-pointer'><FaRegUser />My Profile</p>
+            <p className='flex items-center border-b-2 rounded-sm text-gray-500 hover:text-black hover:border-black transition ease-in-out delay-75 sm:cursor-pointer'><IoShirtOutline />My Orders</p>
+            <p onClick={() => dispatch(setUserLogOut)} className='flex items-center border-b-2 rounded-sm text-gray-500 hover:text-black hover:border-black transition ease-in-out delay-75 sm:cursor-pointer'><BiExit />Log out</p>
           </div>
           <div className='w-[360px]'>
             <h2 className='text-xl font-semibold'>
@@ -45,8 +47,27 @@ const Profile = () => {
           </div>
         </div>
       </div>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
     </Layout>
   )
 }
 
 export default Profile
+
+export async function getServerSideProps(context) {
+  const res = await fetch(`https://.../data`)
+  const user = await res.json()
+
+  if (!user) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}
--- pages/register.js
+++ pages/register.js
@@ -1,8 +1,37 @@
 import Layout from "../components/Layout"
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from "react"
 
 const Register = () => {
+
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const response = await fetch('http://localhost:5000/user/register', {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json()
+    alert(data.msg)
+  }
+
   return (
     <Layout title='Register'>
       <div className='flex mt-10 sm:mt-0'>
@@ -12,21 +41,21 @@ const Register = () => {
         </div>
 
         <div className='flex flex-col m-auto'>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div>
               <h1 className="text-2xl font-bold">Register your account</h1>
             </div>
             <div className="my-3">
               <label className="block text-md mb-2" htmlFor="name">Name</label>
-              <input className="px-4 w-full border-2 py-2 rounded-md text-sm outline-none" type="email" name="name" placeholder="name" />
+              <input onChange={handleName} className="px-4 w-full border-2 py-2 rounded-md text-sm outline-none" type='text' name="name" placeholder="name" />
             </div>
             <div className="my-3">
               <label className="block text-md mb-2" htmlFor="email">Email</label>
-              <input className="px-4 w-full border-2 py-2 rounded-md text-sm outline-none" type="email" name="email" placeholder="email" />
+              <input onChange={handleEmail} className="px-4 w-full border-2 py-2 rounded-md text-sm outline-none" type="email" name="email" placeholder="email" />
             </div>
             <div className="mt-3">
               <label className="block text-md mb-2" htmlFor="password">Password</label>
-              <input className="px-4 w-full border-2 py-2 rounded-md text-sm outline-none" type="password" name="password" placeholder="password" />
+              <input onChange={handlePassword} className="px-4 w-full border-2 py-2 rounded-md text-sm outline-none" type="password" name="password" placeholder="password" />
             </div>
             <div>
               <button className="mt-4 mb-3 w-full bg-black hover:bg-black-900 text-white py-2 rounded-md transition duration-100">Register</button>
