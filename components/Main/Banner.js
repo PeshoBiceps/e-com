8a5c51834491e23642db3dd02cacb23d5216a21a
PeshoBiceps <peshowww@gmail.com>
2021-10-14 13:22:01 +0300

    .

--- components/Main/Banner.js
+++ components/Main/Banner.js
@@ -1,6 +1,10 @@
 import Image from 'next/image'
+import { useRouter } from 'next/router';
 
 const Banner = () => {
+
+  const router = useRouter()
+
   return (
     <div className='relative'>
       <div className='relative min-h-[300px] h-[30vh] md:h-[55vh] w-full'>
@@ -12,8 +16,12 @@ const Banner = () => {
       </div >
       <div className='absolute top-[30%] sm:top-[40%] left-[5%]'>
         <h2 className='text-white font-medium'>For any occasion</h2>
-        <h1 className='text-white text-2xl md:text-4xl font-medium py-2 md:py-6'>Shirt guide</h1>
-        <button className='h-8 w-24 bg-opacity-70 bg-white  group-hover:bg-opacity-10 group-hover:bg-gray-200 group-hover:text-white transition delay-105 ease-out'>Story</button>
+        <h1 className='text-white text-2xl md:text-4xl font-medium py-2 md:py-6'>Browse all products</h1>
+        <button
+          onClick={() => router.push({
+            pathname: '/products',
+          })}
+          className='h-8 w-24 bg-opacity-70 bg-white  group-hover:bg-opacity-10 group-hover:bg-gray-200 group-hover:text-white transition delay-105 ease-out'>View</button>
       </div>
     </div>
 
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,24 +1,72 @@
 import Layout from '../../components/Layout'
 import ProductCard from '../../components/Main/ProductCard'
+
 import { useRouter } from 'next/router';
-import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai'
+import { useState, useEffect } from 'react';
 
 import { dbConnect } from '../../utils/dbConnect'
 import Product from '../../models/productModel'
 
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai'
 
 const Products = ({ products, page, numberOfProducts }) => {
 
   const router = useRouter()
 
-  const lastPage = Math.ceil(numberOfProducts / 6)
+  const [brand, setBrand] = useState('')
+  const [category, setCategory] = useState('')
+  console.log(brand)
+
+  const lastPage = Math.ceil(numberOfProducts / 8)
+
+  const handleChangeBrand = e => {
+    setBrand(e.target.value)
+  }
+  const handleChangeCategory = e => {
+    setCategory(e.target.value)
+  }
+
+  const onChange = () => {
+    router.push({
+      pathname: '/products',
+      query: { brand: brand, category: category }
+    })
+  }
+
+  useEffect(() => {
+    onChange()
+  }, [brand, category])
 
   return (
     <Layout title='Products'>
       <main>
         <section className='max-w-[1200px] w-[90%] mx-auto mt-10'>
+          <div className=' ml-2 my-4'>
+
+            <div className='flex space-x-2'>
+
+              <div>
+                <select value={category} onChange={handleChangeCategory} className="bg-gray-200 p-2 focus:outline-none shadow-sm" id="category">
+                  <option value='shirts'>Shirts</option>
+                  <option value='pants'>Pants</option>
+                  <option value='hats'>Hats</option>
+                </select>
+              </div>
+
+              <div>
+                <select value={brand} onChange={handleChangeBrand} className="bg-gray-200 p-2 focus:outline-none shadow-sm" id="brand">
+                  <option>Nike</option>
+                  <option>Adidas</option>
+                  <option>Boss</option>
+                </select>
+              </div>
+            </div>
+
+
+
+          </div>
           <ProductCard products={products} />
-          <div className='flex items-center justify-around h-10'>
+          <div className='flex items-center justify-around h-10 mt-4'>
             <button onClick={() => router.push(`/products?page=${page - 1}`)} disabled={page <= 1} className='bg-black text-white px-4 py-1 shadow-sm'><AiOutlineLeft /></button>
             <p className='text-xs'>{page}</p>
             <button onClick={() => router.push(`/products?page=${page + 1}`)} disabled={page >= lastPage} className='bg-black text-white px-4 py-1 shadow-sm'><AiOutlineRight /></button>
@@ -33,12 +81,12 @@ export default Products
 
 export async function getServerSideProps({ query: { page = 1 } }) {
 
-  const start = +page === 1 ? 0 : (+page - 1) * 6
+  const start = +page === 1 ? 0 : (+page - 1) * 8
 
   await dbConnect()
 
   const numberOfProducts = await Product.estimatedDocumentCount()
-  const data = await Product.find().limit(6).skip(start)
+  const data = await Product.find().limit(8).skip(start)
 
 
   return {
