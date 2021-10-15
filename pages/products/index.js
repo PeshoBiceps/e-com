29d042f559bba6dfd38908bef8337f877e1741c0
PeshoBiceps <peshowww@gmail.com>
2021-10-15 19:42:28 +0300

    .

--- pages/products/index.js
+++ pages/products/index.js
@@ -21,8 +21,10 @@ const Products = ({ products, page, numberOfProducts }) => {
   const handleChangeBrand = e => {
     setBrand(e.target.value)
   }
-  const handleChangeCategory = e => {
-    setCategory(e.target.value)
+
+  const allProducts = () => {
+    setBrand('')
+    setCategory('')
   }
 
   const onChange = () => {
@@ -38,41 +40,61 @@ const Products = ({ products, page, numberOfProducts }) => {
 
   return (
     <Layout title='Products'>
+
+
       <main>
-        <section className='max-w-[1200px] w-[90%] mx-auto mt-10'>
-          <div className=' ml-2 my-4'>
+        <section className='max-w-[1300px] w-[90%] mx-auto mt-2 sm:mt-10' >
+          <div className='flex flex-col sm:flex-row'>
 
-            <div className='flex space-x-2'>
-
-              <div>
-                <select value={category} onChange={handleChangeCategory} className="bg-gray-200 p-2 focus:outline-none shadow-sm" id="category">
-                  <option value='shirts'>Shirts</option>
-                  <option value='pants'>Pants</option>
-                  <option value='hats'>Hats</option>
-                </select>
+            <div className=' sm:w-[130px] px-2 py-4 space-x-3'>
+              <h1 className='font-semibold mb-2 sm:border-b cursor-pointer' onClick={allProducts}>All products</h1>
+              <div className='text-sm flex justify-around sm:flex-col'>
+                <p className='border-b sm:border-none cursor-pointer' onClick={() => setCategory('shirts')}>Shirts</p>
+                <p className='border-b sm:border-none cursor-pointer' onClick={() => setCategory('pants')}>Pants</p>
+                <p className='border-b sm:border-none cursor-pointer' onClick={() => setCategory('hats')}>Hats</p>
               </div>
 
-              <div>
-                <select value={brand} onChange={handleChangeBrand} className="bg-gray-200 p-2 focus:outline-none shadow-sm" id="brand">
-                  <option>Nike</option>
-                  <option>Adidas</option>
-                  <option>Boss</option>
-                </select>
-              </div>
             </div>
 
+            <div className='flex-col flex-grow mt-1 sm:mt-3'>
 
+              <div className='flex px-2 space-x-1'>
+                <div>
+                  <select onChange={handleChangeBrand} className="bg-gray-200 p-1 text-sm focus:outline-none focus:bg-gray-200 shadow-sm" id="brand">
+                    <option selected disabled hidden>Choose a brand</option>
+                    <option>Nike</option>
+                    <option>Adidas</option>
+                    <option>Karl Lagerfeld</option>
+                    <option>HUGO</option>
+                    <option>Calvin Klein</option>
+                  </select>
+                </div>
+              </div>
 
-          </div>
-          <ProductCard products={products} />
-          <div className='flex items-center justify-around h-10 mt-4'>
-            <button onClick={() => router.push(`/products?page=${page - 1}`)} disabled={page <= 1} className='bg-black text-white px-4 py-1 shadow-sm'><AiOutlineLeft /></button>
-            <p className='text-xs'>{page}</p>
-            <button onClick={() => router.push(`/products?page=${page + 1}`)} disabled={page >= lastPage} className='bg-black text-white px-4 py-1 shadow-sm'><AiOutlineRight /></button>
+              {products.data.length === 0 ? (<div>No products found</div>) :
+                (
+                  <>
+                    <ProductCard products={products} />
+                    <div className='flex items-center justify-around h-10 mt-4'>
+                      <button onClick={() => router.push({
+                        pathname: '/products',
+                        query: { brand: brand, category: category, page: `${page - 1}` }
+                      })}
+                        disabled={page <= 1}
+                        className='bg-black text-white px-4 py-1 shadow-sm'><AiOutlineLeft /></button>
+                      <p className='text-xs'>{page}</p>
+                      <button onClick={() => router.push({
+                        pathname: '/products',
+                        query: { brand: brand, category: category, page: `${page + 1}` }
+                      })} disabled={page >= lastPage} className='bg-black text-white px-4 py-1 shadow-sm'><AiOutlineRight /></button>
+                    </div>
+                  </>
+                )}
+            </div>
           </div>
         </section>
       </main>
-    </Layout>
+    </Layout >
   )
 }
 
@@ -88,12 +110,8 @@ export async function getServerSideProps({ query }) {
   await dbConnect()
 
   const numberOfProducts = await Product.estimatedDocumentCount()
-  const filterCategory = category ? { category: category } : {}
-  const filterBrand = brand ? { brand: brand } : {}
-  const a = category && brand ? { brand: brand, category: category } : {}
 
-  const filter = category && brand ? { category: category, brand: brand } : brand ? { brand: brand } : category ? { category: category } : {}
-  console.log(filter)
+  const filter = category && brand ? { category: category, brand: brand } : category ? { category: category } : brand ? { brand: brand } : {}
   const data = await Product.find(filter).limit(8).skip(start)
 
   return {
