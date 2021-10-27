29fc16868dca100e7fa6959e29a149a42b49fd85
PeshoBiceps <peshowww@gmail.com>
2021-10-27 23:35:34 +0300

    .

--- models/orderModel.js
+++ models/orderModel.js
@@ -4,36 +4,14 @@ const orderSchema = new mongoose.Schema(
   {
     email: {
       type: String,
-      required: true
     },
     totalAmount: {
       type: Number,
-      required: true
     },
-    items: [
-      {
-        productId: {
-          type: String,
-          required: true,
-        },
-        productName: {
-          type: String,
-          required: true,
-        },
-        price: {
-          type: Number,
-          required: true,
-        },
-        qty: {
-          type: Number,
-          required: true,
-        }
-      }
-    ],
+    images: [String],
     paymentType: {
       type: String,
       enum: ["cash", "card"],
-      required: true,
     }
   },
   { timestamps: true }
---
+++ pages/api/webhook.js
@@ -0,0 +1,55 @@
+import { buffer } from "micro"
+
+import { dbConnect } from '../../utils/dbConnect'
+import Order from '../../models/orderModel'
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+
+const endpointSecret = process.env.STRIPE_SIGNIN_SECRET
+
+const fulfillOrder = async (session) => {
+  console.log('Fulfilling order', session)
+  console.log(session.customer_details.email, session.amount_total, JSON.parse(session.metadata.images))
+  await dbConnect()
+  const order = new Order({
+    email: session.customer_details.email,
+    totalAmount: session.amount_total * 100,
+    images: JSON.parse(session.metadata.images),
+    paymentType: JSON.parse(session.payment_method_types)
+  })
+  await order.save()
+
+
+}
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+
+    const rawBody = await buffer(req);
+    const signature = req.headers['stripe-signature']
+
+    let event
+
+    //Verify that the event posted came from stripe 
+    try {
+      event = stripe.webhooks.constructEvent(rawBody.toString(), signature, endpointSecret)
+    } catch (err) {
+      console.log("ERROR", err.message)
+      return res.status(400).send(`Webhook error: ${err.message}`)
+    }
+
+    //Handle the checkout session 
+    if (event.type === 'checkout.session.completed') {
+      const session = event.data.object;
+
+      //Fulfill the order
+      return fulfillOrder(session).then(() => res.status(200)).catch(err => res.status(400).send(`Webhook Error: ${err.message}`))
+    }
+  }
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
